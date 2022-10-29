9321094eb8278194100b20f19202a321999ed945
Ravi Nagi <rasvindra@hotmail.com>
2022-10-29 10:40:02 -0700

    writing function to add class to create color effect based on current time

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,27 @@ document.getElementById("currentDay").innerHTML = exactTime;
 }
 currentMoment();
 setInterval(currentMoment, 200);
+
+
+
+
+
+function colorEffect() {
+    var hour = moment().hours();
+
+    $(".time-block").each( fucnction() {
+    var workHour = parseInt(this).parent().attr("id"));
+
+        if (workHour < hour) {
+            $(this).addClass("past");
+        } else if ( workHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+
+    })
+
+ })
+
+}
